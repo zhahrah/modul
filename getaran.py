33c59dfb074289e7c6651a93bcdf3c9a54581d4d
zhahrah <49123237+zhahrah@users.noreply.github.com>
2019-10-16 19:19:53 +0800

    Create getaran.py

---
+++ getaran.py
@@ -0,0 +1,15 @@
+# Nama file : Periode&frekuensigetaran.py
+
+import math
+
+# periode getaran
+def periodegetaran(t, n):
+  return t/n
+
+# frekuensi getaran
+def frekuensigetaran(n, t):
+  return n/t
+
+# gelombang getaran
+def gelombanggetaran(a, f):
+  return a/f
